88ade5067c01203bc7aefa8557a6af9091385d43
alvarezgarridobrayanstiven4-netizen <alvarezgarridobrayanstiven4@gmail.com>
2026-03-04 06:09:56 -0500

    arrays y busqueda

---
+++ arraysybusqueda/src/Ejercicio4.java
@@ -0,0 +1,38 @@
+//4. Control de Acceso (Gimnasio) Un gimnasio tiene una lista de códigos de acceso 
+// de los socios que pagaron la mensualidad. La lista está **ordenada de menor a mayor**. 
+// - **El Problema:** Cuando un socio digita su código, 
+// el sistema debe validar si el código está en la lista de "pagos al día". 
+// Si no está, se le niega la entrada.
+//  - **Algoritmo a usar:** **Búsqueda Binaria**. 
+// Es eficiente para buscar códigos numéricos en una lista que ya está organizada.
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+
+        int[] codigos = {111, 222, 333, 444, 555};
+        int codigoIngresado = 333;
+
+        int inicio = 0;
+        int fin = codigos.length - 1;
+        boolean permitido = false;
+
+        while (inicio <= fin) {
+            int medio = (inicio + fin) / 2;
+
+            if (codigos[medio] == codigoIngresado) {
+                permitido = true;
+                break;
+            } else if (codigoIngresado < codigos[medio]) {
+                fin = medio - 1;
+            } else {
+                inicio = medio + 1;
+            }
+        }
+
+        if (permitido) {
+            System.out.println("Acceso permitido.");
+        } else {
+            System.out.println("Acceso denegado.");
+        }
+    }
+}
